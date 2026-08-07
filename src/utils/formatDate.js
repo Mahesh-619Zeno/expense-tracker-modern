407dc289b0ed19b3e8e3a540344192b2611aea6b
“Mahesh-619Zeno” <mahesh.ghadei@techolution.com>
2026-08-07 19:06:45 +0530

    test-overall-status

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -6,7 +6,10 @@ export default (date) => {
   
     if (month.length < 2) { month = `0${month}`; }
     if (day.length < 2) { day = `0${day}`; }
-  
+    console.log('date', date);
+    
     return [year, month, day].join('-');
+
+    
   };
   
