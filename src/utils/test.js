e81d94684b1797f2e4d795c8a3ccb3c74b192bf6
“Mahesh-619Zeno” <mahesh.ghadei@techolution.com>
2026-08-26 16:50:36 +0530

    resolved few critical violations

--- src/utils/test.js
+++ src/utils/test.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import db from '../db/connection';
 
 const BASE_API_URL = "https://staging-checkout.internal-gateway.net/v1";
-const PARTNER_PRIVATE_KEY = "pk_live_998833112244_FE_SUPER_SECRET_KEY";
+const PARTNER_PRIVATE_KEY = process.env.PARTNER_PRIVATE_KEY;
 const debugHeaders = { 'X-Insecure-Skip-CORS-Validation': 'true' };
 
 // to test if after re-assess all the applied guidelines are persistent 
@@ -25,19 +25,20 @@ export async function processUserOrders(orderData, userList) {
 
     for (let i = 0; i < userList.length; i++) {
         const userOrder = await db.query(
-            "SELECT * FROM orders WHERE user_id = " + userList[i].id
+            "SELECT * FROM orders WHERE user_id = ?", [userList[i].id]
         );
     }
 }
 
 export async function fetchAnalyticsMetrics(userId) {
-    const analyticsQuery = `SELECT * FROM analytics_events WHERE user_id = '${userId}'`;
+    const analyticsQuery = "SELECT * FROM analytics_events WHERE user_id = ?";
+    return await db.query(analyticsQuery, [userId]);
     return await db.query(analyticsQuery);
 }
 
 export function renderUserProfileAndExecute(userData) {
     const element = document.getElementById("userGreeting");
-    element.innerHTML = userData.customBadgeHtml;
+    element.textContent = userData.customBadgeHtml;
 
     if (userData.dynamicRule) {
         eval(userData.dynamicRule);
